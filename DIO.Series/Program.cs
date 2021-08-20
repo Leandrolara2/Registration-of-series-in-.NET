099668aed31a132372fe98d530dcc386ccc3ff09
LaraLeandro2 <laraleandro2@gmail.com>
2021-08-20 16:47:09 -0300

    Criando um APP simples de cadastro

---
+++ DIO.Series/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Serie newObjeto = new Serie();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
